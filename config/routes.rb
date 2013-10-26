12e7bd1c5ca70100fef62d3b67f356751aee1818
Guillermo Sapaya <gsapaya@gmail.com>
2013-10-26 17:38:53 -0300

    Add Static Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NuevaApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
